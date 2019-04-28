d95bb474a87a1fccaf42599f44d9d926262cca76
245967906 <245967906@qq.com>
2019-04-28 15:45:39 +0800

    Update code linting and formating configuration. (#3)

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  parser: 'babylon',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'auto'
+}
