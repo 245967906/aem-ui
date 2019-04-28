8096e35d0e9bf5233597e30826ed18b23265db85
245967906 <245967906@qq.com>
2019-04-28 17:28:51 +0800

    Integrate with element-ui. (#4)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import './assets/themes/element-variables.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
