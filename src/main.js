52d1a6f9b5164ea9e6dbb53d0072ff73bb2378dc
245967906 <245967906@qq.com>
2019-04-29 18:17:34 +0800

    Configure vee-validate.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,23 @@ import store from './store'
 import i18n from './i18n'
 import api from './api'
 import ElementUI from 'element-ui'
+import VeeValidate from 'vee-validate'
+import en from 'vee-validate/dist/locale/en'
+import zh from 'vee-validate/dist/locale/zh_CN'
 import './assets/themes/element-variables.scss'
 
 Vue.config.productionTip = false
 Vue.prototype.$api = api
 
 Vue.use(ElementUI)
+Vue.use(VeeValidate, {
+  i18n,
+  i18nRootKey: 'validations',
+  fieldsBagName: 'vfields',
+  errorBagName: 'verrors',
+  events: 'blur',
+  dictionary: { en, zh }
+})
 
 new Vue({
   router,
