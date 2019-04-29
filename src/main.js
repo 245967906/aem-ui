f41ad4ce2759327a65c93768d1a785d11a6fe4de
245967906 <245967906@qq.com>
2019-04-29 11:13:58 +0800

    Add i18n support.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 import ElementUI from 'element-ui'
 import './assets/themes/element-variables.scss'
 
@@ -12,5 +13,6 @@ Vue.use(ElementUI)
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
