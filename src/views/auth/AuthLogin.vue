57948c1946e646a9f3c228a7f063d4fb69fa2e06
245967906 <245967906@qq.com>
2019-05-05 11:26:56 +0800

    Add AuthLogin.vue.

---
+++ src/views/auth/AuthLogin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login-container">
+    <div class="login-wrapper">
+      <auth-login-logo></auth-login-logo>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthLoginLogo from './components/AuthLoginLogo'
+export default {
+  name: 'AuthLogin',
+  components: {
+    AuthLoginLogo
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.login-container
+  height 100%
+  display flex
+  justify-content center
+  align-items center
+  .login-wrapper
+    display flex
+    flex-direction column
+    align-items center
+    width 300px
+    min-height 350px
+</style>
