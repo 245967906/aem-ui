0dcce3f61fc44f9035f6b0a7861383d2d7a9f5de
245967906 <245967906@qq.com>
2019-05-07 15:01:45 +0800

    Add the global AppBreadcrumb component.

---
+++ src/components/AppBreadcrumb.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-breadcrumb
+    class="breadcrumb-container"
+    separator-class="el-icon-arrow-right"
+  >
+    <el-breadcrumb-item
+      v-for="(item, index) in breadcrumbs"
+      :key="index"
+      :to="item.path"
+    >
+      <span>{{ $t(item.meta.title) }}</span>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'AppBreadcrumb',
+  data () {
+    return {
+      breadcrumbs: this.getBreadcrumbs()
+    }
+  },
+  methods: {
+    getBreadcrumbs () {
+      const matchedRoutes = this.$route.matched
+      const homeRoute = this.$router.options.routes.filter(
+        x => x.name == 'home'
+      )
+      const breadcrumbs =
+        matchedRoutes[0].name != homeRoute[0].name
+          ? homeRoute.concat(matchedRoutes)
+          : matchedRoutes
+      return breadcrumbs
+    }
+  },
+  watch: {
+    $route () {
+      this.breadcrumbs = this.getBreadcrumbs()
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl'
+.breadcrumb-container >>> .el-breadcrumb__inner
+.breadcrumb-container >>> .el-breadcrumb__inner:hover
+  font-weight 400
+  color $hue
+.breadcrumb-container
+  display flex
+  align-items center
+  border-bottom 1px solid #eef
+  padding 0 20px
+  height 30px
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home-container">
     <app-header></app-header>
+    <app-breadcrumb></app-breadcrumb>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader'
+import AppBreadcrumb from '@/components/AppBreadcrumb'
 export default {
   name: 'Home',
   components: {
-    AppHeader
+    AppHeader,
+    AppBreadcrumb
   }
 }
 </script>
